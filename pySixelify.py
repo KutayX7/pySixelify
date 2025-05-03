5b202f82b34ce0245b9aa06e08ff3e0d5eef54b6
KutayX7 <103666634+KutayX7@users.noreply.github.com>
2025-05-03 20:26:34 +0300

    Added repo URL

--- pySixelify.py
+++ pySixelify.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
+# https://github.com/KutayX7/pySixelify
 
 import argparse
 from typing import Literal, List, Tuple
